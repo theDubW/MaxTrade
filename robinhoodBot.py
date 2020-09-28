ae1e65b0ce8951ed5f66736fd4ce29df5e24f9a6
theDubW <maxberniew@gmail.com>
2020-09-28 01:23:01 -0500

    Added basic GUI

--- robinhoodBot.py
+++ robinhoodBot.py
@@ -17,78 +17,92 @@ def getInstrData(option):
     inst = inst[str.rfind(inst, '/'):]
     inst = inst[1:]
     return r.options.get_option_instrument_data_by_id(inst)
+def login():
+    if(k.get_password("MaxTradeBot", "BotUserName") == ""):
+        username = input("Robinhood Email/Username:")
+        password = input("Robinhood Password:")
+        k.set_password("MaxTradeBot", "BotUserName", username)
+        k.set_password("MaxTrade", k.get_password("MaxTradeBot", "BotUserName"), password)
+    else:
+        a = r.authentication.login(username=getUserInfo()["username"],password=getUserInfo()["password"],by_sms=False)
+def getStockHoldings():
+    stock_positions = r.account.build_holdings()
+    positions = {}
+    for stock in stock_positions:
+        positions[stock] = {"quantity":stock_positions[stock]['quantity'], "equity":stock_positions[stock]['equity'], "percent_change":stock_positions[stock]['percent_change']}
+    return positions
+def getOptionPositions():
+    option_positions =  r.options.get_open_option_positions()
+    positions = {}
+    for option in option_positions:
+        info = getOptionInfo(option)
+        instr = getInstrData(option)
+        positions[option['chain_symbol']] = {"strike_price":instr['strike_price'], "type":option['type']+" "+instr['type'],"exp_date":instr['expiration_date'],"quantity":option['quantity'],"percent_change":percentChange(option, info)}
+    return positions
 
-if(k.get_password("MaxTradeBot", "BotUserName") == ""):
-    username = input("Robinhood Email/Username:")
-    password = input("Robinhood Password:")
-    k.set_password("MaxTradeBot", "BotUserName", username)
-    k.set_password("MaxTrade", k.get_password("MaxTradeBot", "BotUserName"), password)
-else:
-    a = r.authentication.login(username=getUserInfo()["username"],password=getUserInfo()["password"],by_sms=False)
-
-stock_positions = r.account.build_holdings()
-option_positions =  r.options.get_open_option_positions()
+# stock_positions = r.account.build_holdings()
+# option_positions =  r.options.get_open_option_positions()
 
 
 #PRINTING CURRENT STOCK POSITIONS
-print("Stock Positions")
-print("Ticker--------------Quantity---------------Equity-------------------Percent Change")
-for stock in stock_positions:
-    print(stock+"                "+stock_positions[stock]['quantity']+"          $"+stock_positions[stock]['equity']+"                         "+stock_positions[stock]['percent_change']+"%")
-print("\n \n \n")
+# print("Stock Positions")
+# print("Ticker--------------Quantity---------------Equity-------------------Percent Change")
+# for stock in stock_positions:
+#     print(stock+"                "+stock_positions[stock]['quantity']+"          $"+stock_positions[stock]['equity']+"                         "+stock_positions[stock]['percent_change']+"%")
+# print("\n \n \n")
 
 #PRINTING CURRENT OPTION POSITIONS
-print("Option Positions")
-print("Ticker--------------Strike Price----------Option Type----------Expiration Date-------Number of Contracts--------Percent Change")
-for option in option_positions:
-    info = getOptionInfo(option)
-    instr = getInstrData(option)
-    # print(option)
-    # print(info)
-    # print(instr)
-    print(option['chain_symbol']+"                   "+str(instr['strike_price'])+"               "+option['type']+" "+instr['type']+"             "+instr['expiration_date']+"             "+option['quantity']+"                    "+str(percentChange(option, info))+"%")
+# print("Option Positions")
+# print("Ticker--------------Strike Price----------Option Type----------Expiration Date-------Number of Contracts--------Percent Change")
+# for option in option_positions:
+#     info = getOptionInfo(option)
+#     instr = getInstrData(option)
+#     # print(option)
+#     # print(info)
+#     # print(instr)
+#     print(option['chain_symbol']+"                   "+str(instr['strike_price'])+"               "+option['type']+" "+instr['type']+"             "+instr['expiration_date']+"             "+option['quantity']+"                    "+str(percentChange(option, info))+"%")
 
-stock_stops = {}
+# stock_stops = {}
 #ASKING FOR STOCK STOP LOSS/GAIN
-print("STOCKS STOP LOSS AND TAKE PROFIT: ")
-for stock in stock_positions:
-    print("---------------------------------------------------------------------------------------------------------------------------------------------")
-    set_stop = input("Would you like to set a stop loss/take profit for "+stock+"? (y/n)")
-    if(set_stop=="y"):
-        stock_stops[stock] = {"stop_loss":0.0, "take_profit":0.0}
-        confirm_loss = "n"
-        stop_loss = 0
-        confirm_gain = "n"
-        take_profit = 0
-        while(confirm_loss != "y"):
-            stop_loss = float(input("What is the stop loss percentage (do not put negative sign), if you do not want to set a stop loss, input 0.\n"))
-            confirm_loss = input("Stop loss set: -"+str(stop_loss)+"%. Confirm: (y/n)\n")
-        stock_stops[stock]["stop_loss"] = stop_loss
-        while(confirm_gain != "y"):
-            take_profit = float(input("What is the take profit percentage (do not put negative sign), if you do not want to set a take profit, input 0.\n"))
-            confirm_gain = input("Take profit set: "+str(take_profit)+"%. Confirm: (y/n)\n")
-        stock_stops[stock]["take_profit"] = take_profit
+# print("STOCKS STOP LOSS AND TAKE PROFIT: ")
+# for stock in stock_positions:
+#     print("---------------------------------------------------------------------------------------------------------------------------------------------")
+#     set_stop = input("Would you like to set a stop loss/take profit for "+stock+"? (y/n)")
+#     if(set_stop=="y"):
+#         stock_stops[stock] = {"stop_loss":0.0, "take_profit":0.0}
+#         confirm_loss = "n"
+#         stop_loss = 0
+#         confirm_gain = "n"
+#         take_profit = 0
+#         while(confirm_loss != "y"):
+#             stop_loss = float(input("What is the stop loss percentage (do not put negative sign), if you do not want to set a stop loss, input 0.\n"))
+#             confirm_loss = input("Stop loss set: -"+str(stop_loss)+"%. Confirm: (y/n)\n")
+#         stock_stops[stock]["stop_loss"] = stop_loss
+#         while(confirm_gain != "y"):
+#             take_profit = float(input("What is the take profit percentage (do not put negative sign), if you do not want to set a take profit, input 0.\n"))
+#             confirm_gain = input("Take profit set: "+str(take_profit)+"%. Confirm: (y/n)\n")
+#         stock_stops[stock]["take_profit"] = take_profit
 
-options_stops = {}
+# options_stops = {}
 #ASKING FOR OPTION STOP LOSS/GAIN
-print("OPTIONS STOP LOSS AND TAKE PROFIT: ")
-for option in option_positions:
-    print("---------------------------------------------------------------------------------------------------------------------------------------------")
-    ticker = option['chain_symbol']
-    set_stop = input("Would you like to set a stop loss/take profit for "+ticker+"? (y/n)")
-    if(set_stop=="y"):
-        options_stops[ticker] = {"stop_loss":0.0, "take_profit":0.0}
-        confirm_loss = "n"
-        stop_loss = 0
-        confirm_gain = "n"
-        take_profit = 0
-        while(confirm_loss != "y"):
-            stop_loss = float(input("What is the stop loss percentage (do not put negative sign), if you do not want to set a stop loss, input 0.\n"))
-            confirm_loss = input("Stop loss set: -"+str(stop_loss)+"%. Confirm: (y/n)\n")
-        options_stops[ticker]["stop_loss"] = stop_loss
-        while(confirm_gain != "y"):
-            take_profit = float(input("What is the take profit percentage (do not put negative sign), if you do not want to set a take profit, input 0.\n"))
-            confirm_gain = input("Take profit set: "+str(take_profit)+"%. Confirm: (y/n)\n")
-        options_stops[ticker]["take_profit"] = take_profit
-print(stock_stops)
-print(options_stops)
+# print("OPTIONS STOP LOSS AND TAKE PROFIT: ")
+# for option in option_positions:
+#     print("---------------------------------------------------------------------------------------------------------------------------------------------")
+#     ticker = option['chain_symbol']
+#     set_stop = input("Would you like to set a stop loss/take profit for "+ticker+"? (y/n)")
+#     if(set_stop=="y"):
+#         options_stops[ticker] = {"stop_loss":0.0, "take_profit":0.0}
+#         confirm_loss = "n"
+#         stop_loss = 0
+#         confirm_gain = "n"
+#         take_profit = 0
+#         while(confirm_loss != "y"):
+#             stop_loss = float(input("What is the stop loss percentage (do not put negative sign), if you do not want to set a stop loss, input 0.\n"))
+#             confirm_loss = input("Stop loss set: -"+str(stop_loss)+"%. Confirm: (y/n)\n")
+#         options_stops[ticker]["stop_loss"] = stop_loss
+#         while(confirm_gain != "y"):
+#             take_profit = float(input("What is the take profit percentage (do not put negative sign), if you do not want to set a take profit, input 0.\n"))
+#             confirm_gain = input("Take profit set: "+str(take_profit)+"%. Confirm: (y/n)\n")
+#         options_stops[ticker]["take_profit"] = take_profit
+# print(stock_stops)
+# print(options_stops)
--- testingGUI.py
+++ testingGUI.py
@@ -1,25 +1,82 @@
-from PySide2.QtWidgets import QApplication, QPushButton
+from PySide2.QtWidgets import QApplication, QPushButton, QDialog, QLineEdit, QPushButton, QVBoxLayout, QLabel, QMainWindow, QAction
 from PySide2.QtQuick import QQuickView
-from PySide2.QtCore import QUrl, Slot
+from PySide2.QtGui import QIcon, QKeySequence, QPixmap
+from PySide2.QtCore import QUrl, Slot, Qt
 
 import sys
+import rc_images
+import robinhoodBot
 
-@Slot()
-def setLossGain():
-    print("Set the stop loss and take profit!")
+class MainWindow(QMainWindow):
+    def __init__(self, form):
+        QMainWindow.__init__(self)
+        self.setWindowTitle("MaxTrade")
+        self.setWindowIcon(QIcon(QPixmap(":/Icons/logo.png")))
+        self.setWindowState(Qt.WindowMaximized)
+        self.setCentralWidget(form)
 
-app = QApplication(sys.argv)
+        self.menu = self.menuBar()
+        self.file_menu = self.menu.addMenu("File")
 
-execute_bot = QPushButton("Set stop loss and gain")
-execute_bot.clicked.connect(setLossGain)
-execute_bot.show()
+        exit_action = QAction("Exit", self)
+        exit_action.setShortcut(QKeySequence.Quit)
+        exit_action.triggered.connect(self.close)
+
+        self.file_menu.addAction(exit_action)
+
+        self.status = self.statusBar()
+        self.status.showMessage("Welcome to MaxTrade")
+
+        # geometry = QApplication.instance().desktop().availableGeometry(self)
+        # self.setFixedSize(geometry.width() * 1.0, geometry.height() * 1.0)
+
+class Form(QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        
+        self.inputLabel = QLabel("Enter Trade Info")
+        self.stop_loss = QLineEdit("Stop loss %")
+        self.take_profit = QLineEdit("Take profit %")
+        self.confirm = QPushButton("Confirm") 
+        
+        layout = QVBoxLayout()
+        layout.addWidget(self.inputLabel)
+        layout.addWidget(self.stop_loss)
+        layout.addWidget(self.take_profit)
+        layout.addWidget(self.confirm)
+        self.setLayout(layout)
+
+        self.confirm.clicked.connect(self.confirmLossGain)
+    def confirmLossGain(self):
+        print("Stop Loss: {}\nTake Profit: {}".format(self.stop_loss.text(), self.take_profit.text()))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = Form()
+    main = MainWindow(form)
+    main.show()
+
+    with open("style.qss", "r") as f:
+        _style = f.read()
+        app.setStyleSheet(_style)
+    sys.exit(app.exec_())
+
+# @Slot()
+# def confirmLossGain():
+#     print("")
+
+
+
+#button e.g.
+# app = QApplication(sys.argv)
+# execute_bot = QPushButton("Set stop loss and gain")
+# execute_bot.clicked.connect(setLossGain)
+# execute_bot.show()
 
 #QML E.G.
 # view = QQuickView()
 # url = QUrl("bot.qml")
-
-
 # view.setSource(url)
 # view.setResizeMode(QQuickView.SizeRootObjectToView)
 # view.show()
-app.exec_()
+# app.exec_()
